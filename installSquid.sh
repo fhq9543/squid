ba6fa82171c39d05d02f559d11bf0db964ef8b55
sandy <sandy@spiritframe.com>
2020-08-15 17:41:10 +0800

    add sh

--- installSquid.sh
+++ installSquid.sh
@@ -2,12 +2,10 @@
 set -e
 yum install -y squid httpd
 cd /etc/squid/
-cp squid.conf squid.conf.bak
-wget github.com/fhq9543/squid/blob/master/squid.conf
-htpasswd -c /etc/squid/passwd oL18oY
-echo -e MYeB9vNH
-echo -e MYeB9vNH
+mv squid.conf squid.conf.bak
+cp ~/squid.conf .
 squid -z
-systemctl start squid
 iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 53128 -j ACCEPT
+htpasswd -c /etc/squid/passwd qvkc91
+systemctl start squid
 
